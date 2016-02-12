91b72ec28c6786ba821f6a0e3aa4c6475f39ece5
Adam Werries <werries05@gmail.com>
2016-02-12 17:16:50 -0500

    split up error metrics and plots functions, added additional plots

---
+++ MATLAB/generate_error_metrics.m
@@ -0,0 +1,36 @@
+
+%% Compute error statistics on groundtruth vs NovAtel
+distance = ((ground_truth(:,1)-gps_x).^2 + (ground_truth(:,2)-gps_y).^2).^0.5;
+raw_rms = rms(distance);
+raw_max = max(distance);
+fprintf('\nRMS 2D Raw: %0.4fm\n',raw_rms)
+fprintf('Max 2D Raw: %0.4fm\n',raw_max)
+
+%% Compute error statistics on groundtruth vs filter output
+xyz = out_profile(:,2:4);
+llh = ecef2lla(xyz);
+[x,y] = deg2utm(llh(:,1),llh(:,2));
+x = x-min_x;
+y = y-min_y;
+h = -llh(:,3)+2;
+
+distance = ((ground_truth_full(:,1)-x).^2 + (ground_truth_full(:,2)-y).^2).^0.5;
+filter_rms = rms(distance);
+filter_max = max(distance);
+fprintf('RMS 2D Filter: %0.4fm, %0.2f%% change\n',filter_rms,(raw_rms-filter_rms)/raw_rms*100);
+fprintf('Max 2D Filter: %0.4fm, %0.2f%% change\n',filter_max,(raw_max-filter_max)/raw_max*100);
+
+%% 3D Stats
+distance = ((ground_truth(:,1)-gps_x).^2 + (ground_truth(:,2)-gps_y).^2 + (ground_truth(:,3)-gps_h).^2).^0.5;
+raw_rms = rms(distance);
+raw_max = max(distance);
+fprintf('\nRMS 3D Raw: %0.4fm\n',raw_rms)
+fprintf('Max 3D Raw: %0.4fm\n',raw_max)
+distance = ((ground_truth_full(:,1)-x).^2 + (ground_truth_full(:,2)-y).^2 + (ground_truth_full(:,3)-h).^2).^0.5;
+filter_rms = rms(distance);
+filter_max = max(distance);
+fprintf('RMS 3D Filter: %0.4fm, %0.2f%% change\n',filter_rms,(raw_rms-filter_rms)/raw_rms*100);
+fprintf('Max 3D Filter: %0.4fm, %0.2f%% change\n',filter_max,(raw_max-filter_max)/raw_max*100);
+
+
+%% Rotation stats
--- MATLAB/generate_plots.m
+++ MATLAB/generate_plots.m
@@ -1,4 +1,27 @@
-%% Compare ground truth to raw NovAtel data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Raw IMU data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figurec;
+subplot(311);
+plot(imu_time, imu(:,2), '.');
+xlabel('Time (s)'); ylabel('X Acceleration (m/s^2)');
+subplot(312);
+plot(imu_time, imu(:,3), '.');
+xlabel('Time (s)'); ylabel('Y Acceleration (m/s^2)');
+subplot(313);
+plot(imu_time, imu(:,4), '.');
+xlabel('Time (s)'); ylabel('Z Acceleration (m/s^2)');
+
+figurec;
+subplot(311);
+plot(imu_time, imu(:,5), '.');
+xlabel('Time (s)'); ylabel('Roll rate (rad/s)');
+subplot(312);
+plot(imu_time, imu(:,6), '.');
+xlabel('Time (s)'); ylabel('Pitch rate (rad/s)');
+subplot(313);
+plot(imu_time, imu(:,7), '.');
+xlabel('Time (s)'); ylabel('Yaw rate (rad/s)');
+
+% %% Compare ground truth to raw NovAtel data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figurec;
 plot(gps_x, gps_y,'r--', 'LineWidth', 2);
 xlabel('Easting (m)');
@@ -31,12 +54,23 @@ hold on; plot(nov_time, ground_truth(:,3),'c');
 legend('NovAtel','Applanix','Location','SouthEast');
 
 %% Plot filter results %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-xyz = out_profile(:,2:4);
-llh = ecef2lla(xyz);
-[x,y] = deg2utm(llh(:,1),llh(:,2));
-x = x-min_x;
-y = y-min_y;
-h = -llh(:,3)-1.5;
+roll = out_profile(:,8);
+pitch = out_profile(:,9);
+yaw = out_profile(:,10);
+figurec;
+subplot(311);
+plot(filter_time, rad2deg(roll));
+xlabel('Time (s)');
+ylabel('Roll (deg)');
+subplot(312);
+plot(filter_time, rad2deg(pitch));
+xlabel('Time (s)');
+ylabel('Pitch (deg)');
+subplot(313);
+plot(filter_time, rad2deg(yaw));
+xlabel('Time (s)');
+ylabel('Yaw (deg)');
+
 figurec;
 plot(x, y,'r--', 'LineWidth', 2);
 xlabel('Easting (m)');
@@ -48,43 +82,49 @@ legend('Filtered','Applanix','NovAtel');
 axis equal;
 
 figurec;
-subplot(211);
+subplot(311);
 plot(filter_time, x,'r--', 'LineWidth', 2);
 xlabel('Time (s)');
 ylabel('Easting (m)');
 hold on; plot(filter_time, ground_truth_full(:,1),'c');
 legend('Filtered','Applanix');
 
-subplot(212);
+subplot(312);
 plot(filter_time, y,'r--', 'LineWidth', 2);
 xlabel('Time (s)');
 ylabel('Northing (m)');
 hold on; plot(filter_time, ground_truth_full(:,2),'c');
 legend('Filtered','Applanix','Location','SouthEast');
 
-% subplot(313);
-% plot(imu_time, h,'r--', 'LineWidth', 2);
-% xlabel('Time (s)');
-% ylabel('Height (m)');
-% hold on; plot(imu_time, ground_truth_full(:,3),'c');
-% legend('Filtered','Applanix','Location','SouthEast');
-%% Compute error statistics on groundtruth vs NovAtel %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-distance = ((ground_truth(:,1)-gps_x).^2 + (ground_truth(:,2)-gps_y).^2).^0.5;
-fprintf('\nRMS Raw:%f\n',rms(distance))
-fprintf('Max Raw:%f\n',max(distance))
+subplot(313);
+plot(filter_time, h,'r--', 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Altitude (m)');
+hold on; plot(filter_time, ground_truth_full(:,3),'c');
+legend('Filtered','Applanix','Location','SouthEast');
 
-%% Compute error statistics on groundtruth vs filter output %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-distance = ((ground_truth_full(:,1)-x).^2 + (ground_truth_full(:,2)-y).^2).^0.5;
-fprintf('\nRMS Filter:%f\n',rms(distance))
-fprintf('Max Filter:%f\n',max(distance))
+zero_line = zeros(length(filter_time), 1);
 figurec;
-subplot(211);
-plot(filter_time, x-ground_truth_full(:,1),'r--', 'LineWidth', 2);
+subplot(311);
+plot(filter_time, x-ground_truth_full(:,1),'r--', 'LineWidth', 2); hold on;
+plot(nov_time, gps_x-ground_truth(:,1), ':g', 'LineWidth', 2);
 xlabel('Time (s)');
 ylabel('Easting error (m)');
-ylim([-10 10])
-subplot(212);
-plot(filter_time, y-ground_truth_full(:,2),'r--', 'LineWidth', 2);
+ylim([-10 10]);
+plot(filter_time, zero_line, '--b');
+
+subplot(312);
+plot(filter_time, y-ground_truth_full(:,2),'r--', 'LineWidth', 2); hold on;
+plot(nov_time, gps_y-ground_truth(:,2), ':g', 'LineWidth', 2);
 xlabel('Time (s)');
 ylabel('Northing error (m)');
-ylim([-10 10])
+ylim([-10 10]);
+plot(filter_time, zero_line, '--b');
+
+subplot(313);
+plot(filter_time, h-ground_truth_full(:,3),'r--', 'LineWidth', 2); hold on;
+plot(nov_time, gps_h-ground_truth(:,3), ':g', 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Altitude error (m)');
+ylim([-10 10]);
+plot(filter_time, zero_line, '--b');
