0f7747495b04b0d025e58fdc55648059ddd5fa78
Adam Werries <werries05@gmail.com>
2016-02-13 19:47:21 -0500

    added additional plots

--- MATLAB/generate_plots.m
+++ MATLAB/generate_plots.m
@@ -1,57 +1,57 @@
-%% Raw IMU data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-figurec;
-subplot(311);
-plot(imu_time, imu(:,2), '.');
-xlabel('Time (s)'); ylabel('X Acceleration (m/s^2)');
-subplot(312);
-plot(imu_time, imu(:,3), '.');
-xlabel('Time (s)'); ylabel('Y Acceleration (m/s^2)');
-subplot(313);
-plot(imu_time, imu(:,4), '.');
-xlabel('Time (s)'); ylabel('Z Acceleration (m/s^2)');
-
-figurec;
-subplot(311);
-plot(imu_time, imu(:,5), '.');
-xlabel('Time (s)'); ylabel('Roll rate (rad/s)');
-subplot(312);
-plot(imu_time, imu(:,6), '.');
-xlabel('Time (s)'); ylabel('Pitch rate (rad/s)');
-subplot(313);
-plot(imu_time, imu(:,7), '.');
-xlabel('Time (s)'); ylabel('Yaw rate (rad/s)');
+% %% Raw IMU data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% figurec;
+% subplot(311);
+% plot(imu_time, imu(:,2), '.');
+% xlabel('Time (s)'); ylabel('X Acceleration (m/s^2)');
+% subplot(312);
+% plot(imu_time, imu(:,3), '.');
+% xlabel('Time (s)'); ylabel('Y Acceleration (m/s^2)');
+% subplot(313);
+% plot(imu_time, imu(:,4), '.');
+% xlabel('Time (s)'); ylabel('Z Acceleration (m/s^2)');
+% 
+% figurec;
+% subplot(311);
+% plot(imu_time, imu(:,5), '.');
+% xlabel('Time (s)'); ylabel('Roll rate (rad/s)');
+% subplot(312);
+% plot(imu_time, imu(:,6), '.');
+% xlabel('Time (s)'); ylabel('Pitch rate (rad/s)');
+% subplot(313);
+% plot(imu_time, imu(:,7), '.');
+% xlabel('Time (s)'); ylabel('Yaw rate (rad/s)');
 
 % %% Compare ground truth to raw NovAtel data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-figurec;
-plot(gps_x, gps_y,'r--', 'LineWidth', 2);
-xlabel('Easting (m)');
-ylabel('Northing (m)');
-hold on;
-plot(ground_truth(:,1),ground_truth(:,2),'c');
-legend('NovAtel','Applanix');
-axis equal;
-
-figurec;
-subplot(311);
-plot(nov_time, gps_x,'r--', 'LineWidth', 2);
-xlabel('Time (s)');
-ylabel('Easting (m)');
-hold on; plot(nov_time, ground_truth(:,1),'c');
-legend('NovAtel','Applanix');
-
-subplot(312);
-plot(nov_time, gps_y,'r--', 'LineWidth', 2);
-xlabel('Time (s)');
-ylabel('Northing (m)');
-hold on; plot(nov_time, ground_truth(:,2),'c');
-legend('NovAtel','Applanix','Location','SouthEast');
-
-subplot(313);
-plot(nov_time, gps_h,'r--', 'LineWidth', 2);
-xlabel('Time (s)');
-ylabel('Northing (m)');
-hold on; plot(nov_time, ground_truth(:,3),'c');
-legend('NovAtel','Applanix','Location','SouthEast');
+% figurec;
+% plot(gps_x, gps_y,'r--', 'LineWidth', 2);
+% xlabel('Easting (m)');
+% ylabel('Northing (m)');
+% hold on;
+% plot(ground_truth(:,1),ground_truth(:,2),'c');
+% legend('NovAtel','Applanix');
+% axis equal;
+% 
+% figurec;
+% subplot(311);
+% plot(nov_time, gps_x,'r--', 'LineWidth', 2);
+% xlabel('Time (s)');
+% ylabel('Easting (m)');
+% hold on; plot(nov_time, ground_truth(:,1),'c');
+% legend('NovAtel','Applanix');
+% 
+% subplot(312);
+% plot(nov_time, gps_y,'r--', 'LineWidth', 2);
+% xlabel('Time (s)');
+% ylabel('Northing (m)');
+% hold on; plot(nov_time, ground_truth(:,2),'c');
+% legend('NovAtel','Applanix','Location','SouthEast');
+% 
+% subplot(313);
+% plot(nov_time, gps_h,'r--', 'LineWidth', 2);
+% xlabel('Time (s)');
+% ylabel('Northing (m)');
+% hold on; plot(nov_time, ground_truth(:,3),'c');
+% legend('NovAtel','Applanix','Location','SouthEast');
 
 %% Plot filter results %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 roll = out_profile(:,8);
@@ -59,15 +59,20 @@ pitch = out_profile(:,9);
 yaw = out_profile(:,10);
 figurec;
 subplot(311);
-plot(filter_time, rad2deg(roll));
+plot(filter_time, rad2deg(roll),'-b'); hold on;
+plot(filter_time, rad2deg(ground_truth_full(:,4)),'--r');
 xlabel('Time (s)');
 ylabel('Roll (deg)');
+legend('Filter','Applanix');
+
 subplot(312);
-plot(filter_time, rad2deg(pitch));
+plot(filter_time, rad2deg(pitch)); hold on;
+plot(filter_time, rad2deg(ground_truth_full(:,5)),'--r');
 xlabel('Time (s)');
 ylabel('Pitch (deg)');
 subplot(313);
-plot(filter_time, rad2deg(yaw));
+plot(filter_time, rad2deg(yaw)); hold on;
+plot(filter_time, rad2deg(ground_truth_full(:,6)),'--r');
 xlabel('Time (s)');
 ylabel('Yaw (deg)');
 
@@ -107,24 +112,27 @@ zero_line = zeros(length(filter_time), 1);
 figurec;
 subplot(311);
 plot(filter_time, x-ground_truth_full(:,1),'r--', 'LineWidth', 2); hold on;
-plot(nov_time, gps_x-ground_truth(:,1), ':g', 'LineWidth', 2);
+plot(nov_time, gps_x-ground_truth(:,1), '.g');
 xlabel('Time (s)');
 ylabel('Easting error (m)');
 ylim([-10 10]);
+legend('Filter','Novatel');
 plot(filter_time, zero_line, '--b');
 
 subplot(312);
 plot(filter_time, y-ground_truth_full(:,2),'r--', 'LineWidth', 2); hold on;
-plot(nov_time, gps_y-ground_truth(:,2), ':g', 'LineWidth', 2);
+plot(nov_time, gps_y-ground_truth(:,2), '.g');
 xlabel('Time (s)');
 ylabel('Northing error (m)');
 ylim([-10 10]);
+legend('Filter','Novatel');
 plot(filter_time, zero_line, '--b');
 
 subplot(313);
 plot(filter_time, h-ground_truth_full(:,3),'r--', 'LineWidth', 2); hold on;
-plot(nov_time, gps_h-ground_truth(:,3), ':g', 'LineWidth', 2);
+plot(nov_time, gps_h-ground_truth(:,3), '.g');
 xlabel('Time (s)');
 ylabel('Altitude error (m)');
 ylim([-10 10]);
+legend('Filter','Novatel');
 plot(filter_time, zero_line, '--b');
